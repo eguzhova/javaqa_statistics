97a1730e18eee08b3d788cfde857037757c97941
eguzhova <81353538+eguzhova@users.noreply.github.com>
2021-04-14 17:02:38 +0300

    Methods added

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,7 +3,7 @@ package ru.netology.stats;
 public class StatsService {
 
     //Метод рассчета суммы продаж за все месяцы
-    public static int salesTotal (int[] sales) {
+    public static int salesTotal(int[] sales) {
         int total = 0;
         for (int sale : sales) {
             total += sale;
@@ -12,14 +12,69 @@ public class StatsService {
     }
 
     //Метод расчета среднего значения продаж за все месяцы
-    public static int salesAverage (int [] sales) {
+    public static int salesAverage(int[] sales) {
         int total = 0;
         int month = 0;
         for (int sale : sales) {
             total += sale;
-            month ++;
+            month++;
         }
         return total / month;
     }
 
+    //Метод нахождения месяца с пиком продаж
+    public static int highestSalesMonth(int[] sales) {
+        int month = 0;
+        int monthCount = 0;
+        int previousSale = sales[1];
+        for (int sale : sales) {
+            monthCount++;
+            if (previousSale <= sale) {
+                month = monthCount;
+                previousSale = sale;
+            }
+        }
+        return month;
+    }
+
+    //Метод нахождения месяца с наименьшими продажами
+    public static int lowestSalesMonth(int[] sales) {
+        int month = 0;
+        int monthCount = 0;
+        int previousSale = sales[1];
+        for (int sale : sales) {
+            monthCount++;
+            if (previousSale >= sale) {
+                month = monthCount;
+                previousSale = sale;
+            }
+        }
+        return month;
+    }
+
+    //Метод вычисления количества месяцев с продажами выше среднего
+    public static int aboveAverageSalesMonths(int[] sales) {
+        StatsService service = new StatsService();
+        int average = service.salesAverage (sales);
+        int month = 0;
+        for (int sale : sales) {
+            if (sale > average) {
+                month ++;
+            }
+        }
+        return month;
+    }
+
+    //Метод вычисления количества месяцев с продажами ниже среднего
+    public static int underAverageSalesMonths (int[] sales) {
+        StatsService service = new StatsService();
+        int average = service.salesAverage (sales);
+        int month = 0;
+        for (int sale : sales) {
+            if (sale < average) {
+                month ++;
+            }
+        }
+        return month;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -32,7 +32,67 @@ class StatsServiceTest {
         int expected = 15;
 
         // вызываем целевой метод:
-        int actual = service.salesAverage (sales);
+        int actual = service.salesAverage(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindHighestSalesMonth() {
+        StatsService service = new StatsService();
+
+        // подготавливаем данные:
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        // вызываем целевой метод:
+        int actual = service.highestSalesMonth(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindLowestSalesMonth() {
+        StatsService service = new StatsService();
+
+        // подготавливаем данные:
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        // вызываем целевой метод:
+        int actual = service.lowestSalesMonth(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAboveAverageSalesMonths() {
+        StatsService service = new StatsService();
+
+        // подготавливаем данные:
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        // вызываем целевой метод:
+        int actual = service.aboveAverageSalesMonths(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void underAverageSalesMonths() {
+        StatsService service = new StatsService();
+
+        // подготавливаем данные:
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        // вызываем целевой метод:
+        int actual = service.underAverageSalesMonths(sales);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
